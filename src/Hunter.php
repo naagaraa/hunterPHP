da156543f4b9cddc84c4f2e89464372e367df7b9
naagaraa <ekabersinar@gmail.com>
2021-09-14 15:24:10 +0700

    update readme

--- src/Hunter.php
+++ src/Hunter.php
@@ -1,5 +1,33 @@
 <?php
 
+/**
+ * 
+ * this file is single method of PHP Weight Product
+ * 
+ * 
+ * @author      Eka Jaya Nagara     
+ * @copyright   Copyright (c), 2021 naagaraa HunterPHP simple Journal Title Scraping
+ * @license     MIT public license
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 namespace HunterPHP;
 
 use Goutte\Client;
@@ -16,7 +44,14 @@ class Hunter
         return $this->keyword;
     }
 
-    public function FilterData($webpages, $key = "")
+    /**
+     * FilterData for indetify type of support webpages keyword
+     *
+     * @param string $webpages
+     * @param string $key
+     * @return void
+     */
+    public function FilterData($webpages = "", $key = "")
     {
         switch ($webpages) {
             case 'google_scholar':
@@ -60,6 +95,13 @@ class Hunter
         }
     }
 
+    /**
+     * method for get scrap data and extraction
+     *
+     * @param string $webpages
+     * @param string $key
+     * @return void
+     */
     public function scrap($webpages = "", $key = "")
     {
         // check if webpages not support at the list data
